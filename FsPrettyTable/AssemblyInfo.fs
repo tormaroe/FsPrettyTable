4542dd66acd541110cdf0669d023adf286caa8db
Torbjørn Marø <torbjorn.maro@gmail.com>
2015-01-31 12:47:06 +0100

    Building nuget pack using fake.

---
+++ FsPrettyTable/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyProductAttribute("FsPrettyTable")>]
+[<assembly: AssemblyDescriptionAttribute("Represent tabular data in visually appealing ASCII tables using F#")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","d8123721eba62674b7dc4362aa6067598afc3d0c")>]
+[<assembly: ComVisibleAttribute(false)>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
--- build.fsx
+++ build.fsx
@@ -1,16 +1,43 @@
 // include Fake lib
 #r @"FsPrettyTable/packages/FAKE/tools/FakeLib.dll"
 open Fake
+open Fake.Git
 
 RestorePackages()
 
-// Properties
+let authors = ["Torbjørn Marø"]
+
+// project name and description
+let projectName = "FsPrettyTable"
+let projectDescription = "Represent tabular data in visually appealing ASCII tables using F#"
+let projectSummary = projectDescription // TODO: write a summary
+
 let buildDir = "./build/"
 let testDir  = "./test/"
+let packagingRoot = "./packaging/"
+let packagingDir = packagingRoot @@ "FsPrettyTable"
+
+let releaseNotes = 
+    ReadFile "ReleaseNotes.md"
+    |> ReleaseNotesHelper.parseReleaseNotes
+
+let commitHash = Information.getCurrentSHA1 "."
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDir buildDir
+    CleanDirs [buildDir; testDir; packagingRoot; packagingDir]
+)
+
+open Fake.AssemblyInfoFile
+
+Target "AssemblyInfo" (fun _ ->
+    CreateFSharpAssemblyInfo "FsPrettyTable/AssemblyInfo.fs"
+      [ Attribute.Product projectName
+        Attribute.Description projectDescription
+        Attribute.Version releaseNotes.AssemblyVersion
+        Attribute.FileVersion releaseNotes.AssemblyVersion
+        Attribute.Metadata("githash", commitHash)
+        Attribute.ComVisible false ]
 )
 
 Target "BuildLib" (fun _ ->
@@ -30,20 +57,47 @@ Target "Test" (fun _ ->
       |> NUnit (fun p ->
           {p with
              DisableShadowCopy = true
-             ToolPath = "C:\\Program Files (x86)\\NUnit 2.6.4\\bin\\"
+             ToolPath = @"C:\Program Files (x86)\NUnit 2.6.4\bin\"
              OutputFile = testDir + "TestResults.xml" })
 )
 
-// Default target
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
+Target "CreatePackage" (fun _ ->
+    // Copy all the package files into a package folder
+    CopyFile packagingDir (buildDir @@ "FsPrettyTable.dll")
+    CopyFile packagingDir (buildDir @@ "FsPrettyTable.pdb")
+    CopyFile packagingDir (buildDir @@ "FsPrettyTable.XML")
+    CopyFiles packagingDir  ["LICENSE"; "README.md"; "ReleaseNotes.md"]
+
+    NuGet (fun p -> 
+        {p with
+            Authors = authors
+            Project = projectName
+            Description = projectDescription                               
+            OutputPath = packagingRoot
+            Summary = projectSummary
+            WorkingDir = packagingDir
+            Version = releaseNotes.AssemblyVersion
+            ReleaseNotes = toLines releaseNotes.Notes
+            AccessKey = getBuildParamOrDefault "nugetkey" ""
+            Files = [("FsPrettyTable.dll", Some @"lib\net45\", None)
+                     ("FsPrettyTable.pdb", Some @"lib\net45\", None)
+                     ("FsPrettyTable.XML", Some @"lib\net45\", None)
+                     ("LICENSE", None, None)
+                     ("README.md", None, None)
+                     ("ReleaseNotes.md", None, None)]
+            Publish = hasBuildParam "nugetkey" }) 
+            "FsPrettyTable.nuspec"
 )
+
+Target "Default" DoNothing
 
 // Dependencies
 "Clean"
+  ==> "AssemblyInfo"
   ==> "BuildLib"
   ==> "BuildTests"
   ==> "Test"
+  ==> "CreatePackage"
   ==> "Default"
 
 // start build
